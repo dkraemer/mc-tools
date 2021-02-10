613562bf247d4e993fb0cbe38c9365ceef984dae
Daniel Kraemer <dkraemer@dkross.org>
2021-02-10 22:36:34 +0100

    Implement manifest validation

--- src/curse-manifest.ts
+++ src/curse-manifest.ts
@@ -29,3 +29,39 @@ export class Manifest extends CommonBase {
   files: FileManifest[] = [];
   overrides = 'overrides';
 }
+
+function validateFileManifests(fileManifests: FileManifest[]): boolean {
+  for (const file of fileManifests) {
+    if (typeof file.projectID !== 'number') {
+      return false;
+    }
+    if (typeof file.fileID !== 'number') {
+      return false;
+    }
+    if (typeof file.required !== 'boolean') {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function validateManifest(manifest: Manifest): boolean {
+  return (manifest !== undefined)
+    && (manifest.minecraft !== undefined)
+    && (typeof manifest.minecraft.version === 'string')
+    && (manifest.minecraft.modLoaders)
+    && (manifest.minecraft.modLoaders instanceof Array)
+    && (manifest.minecraft.modLoaders.length > 0)
+    && (typeof manifest.minecraft.modLoaders[0].id === 'string')
+    && (typeof manifest.minecraft.modLoaders[0].primary === 'boolean')
+    && (typeof manifest.manifestType === 'string')
+    && (typeof manifest.manifestVersion === 'number')
+    && (typeof manifest.name === 'string')
+    && (typeof manifest.version === 'string')
+    && (typeof manifest.author === 'string')
+    && (manifest.files)
+    && (manifest.files instanceof Array)
+    && (manifest.files.length > 0)
+    && validateFileManifests(manifest.files)
+    && (typeof manifest.overrides === 'string');
+}
